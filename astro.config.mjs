913e744b15c570b3914968848b332076742e851d
amel linda <Amel@MacBook-Air.local>
2025-11-28 09:06:26 +0700

    initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,25 @@
+import { defineConfig, fontProviders } from "astro/config";
+
+import tailwindcss from "@tailwindcss/vite";
+
+// https://astro.build/config
+export default defineConfig({
+    experimental: {
+        fonts: [
+            {
+                provider: fontProviders.google(),
+                name: "Inter",
+                cssVariable: "--font-inter",
+            },
+            {
+                provider: fontProviders.google(),
+                name: "Fraunces",
+              cssVariable: "--font-fraunces",
+                weights: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+            },
+        ],
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+});
